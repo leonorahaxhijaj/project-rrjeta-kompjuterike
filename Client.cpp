8ea06378da337395be91c3b5bd7f8d61ed3c8fb6
leonismorina <leonis.morina@student.uni-pr.edu>
2026-04-17 15:14:51 +0200

    shtova pjese te klienti
    i vendosa disa pjese te pjesa e klientit

--- Client.cpp
+++ Client.cpp
@@ -1,29 +1,64 @@
-#include <iostream>
-#include <string>
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <iostream> 
+#include <string>  
+#include <winsock2.h>  
+#include <ws2tcpip.h>  
+#include <fstream>  
+#include <vector> 
+#include <map>
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
 
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <ctime>
+
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+int tcp_port = 7777;
+int max_clients = 6;
+
 int main() {
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(7777);
-    inet_pton(AF_INET, "172.16.108.31", &addr.sin_addr);
-
-    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
-        cout << "Lidhja deshtoi!" << endl;
+    WSADATA data;
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
+        cout << "WSAStartup failed\n";
         return 1;
     }
 
-    cout << "U lidhet me Serverin!" << endl;
+    SOCKET main_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (main_socket == INVALID_SOCKET) {
+        cout << "Socket creation failed\n";
+        return 1;
+    }
 
-    closesocket(sock);
+    sockaddr_in main_addr{};
+    main_addr.sin_family = AF_INET;
+    main_addr.sin_addr.s_addr = INADDR_ANY;
+    main_addr.sin_port = htons(tcp_port);
+
+    if (bind(main_socket, (sockaddr*)&main_addr, sizeof(main_addr)) < 0) {
+        cout << "Bind failed\n";
+        return 1;
+    }
+
+    listen(main_socket, max_clients);
+
+    cout << "Serveri duke punuar ne portin " << tcp_port << endl;
+
+    while (true) {
+        SOCKET client_socket = accept(main_socket, NULL, NULL);
+        if (client_socket != INVALID_SOCKET) {
+            cout << "Klient u lidh!" << endl;
+        }
+    }
+
+    closesocket(main_socket);
     WSACleanup();
     return 0;
 }
