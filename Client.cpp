08adec8c374e9a4e2bab2e94475a626a457e1a22
Lindi1111 <mjekiqil494@gmail.com>
2026-04-18 18:11:00 +0200

    klienti - komplet

--- Client.cpp
+++ Client.cpp
@@ -9,11 +9,26 @@
 using namespace std;
 
 int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(7777);
+    inet_pton(AF_INET, "172.16.108.31", &addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+        cout << "Lidhja deshtoi!" << endl;
+        return 1;
+    }
+
+    cout << "U lidhet me Serverin!" << endl;
 
     string line;
     while (true) {
         cout << "Komanda: ";
-        getline(cin, line);
+        getline(cin, line); 
         if (line == "exit") break;
 
         string to_send = line;
@@ -29,28 +44,27 @@ int main() {
             }
         }
 
-        int s = send(sock, to_send.c_str(), (int)to_send.length(), 0);
+    int s = send(sock, to_send.c_str(), (int)to_send.length(), 0);
 
-        if (s == SOCKET_ERROR) {
-            cout << "Lidhja ka rene (Timeout). Duke u rilidhur automatikisht..." << endl;
-
-            closesocket(sock);
-            sock = socket(AF_INET, SOCK_STREAM, 0);
-
-            if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) != SOCKET_ERROR) {
-                cout << "Rilidhja e suksesshme!" << endl;
-                send(sock, to_send.c_str(), (int)to_send.length(), 0);
-            } else {
-                cout << "Serveri nuk po pergjigjet. Provo me vone." << endl;
-                continue;
-            }
+    if (s == SOCKET_ERROR) {
+        cout << "Lidhja ka rene (Timeout). Duke u rilidhur automatikisht..." << endl;
+        
+        closesocket(sock); 
+        sock = socket(AF_INET, SOCK_STREAM, 0); 
+        
+        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) != SOCKET_ERROR) {
+            cout << "Rilidhja e suksesshme!" << endl;
+            send(sock, to_send.c_str(), (int)to_send.length(), 0); 
+        } else {
+            cout << "Serveri nuk po pergjigjet. Provo me vone." << endl;
+            continue; 
         }
+    }
 
-        char buf[4096] = {0};
+        char buf[4096] = {0}; 
         int r = recv(sock, buf, 4096, 0);
         if (r > 0) cout << "Serveri: " << buf << endl;
     }
-
     closesocket(sock);
     WSACleanup();
     return 0;
